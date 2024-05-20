e624669c0714c3c7cece39b1585fea1159e694e9
krepanna <krepanna@gmail.com>
2024-05-20 15:48:48 -0400

    booking form: state added, lifted up, useducer added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import React, { useReducer } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
@@ -5,14 +6,75 @@ import Homepage from './components/Homepage';
 import Reservations from './components/Reservations';
 import { Routes, Route } from 'react-router-dom';
 
+const hours = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+const initialFormState = {
+  date: '',
+  hours: { hours },
+};
+
+function reducer(formState, action) {
+  switch (action.type) {
+    case 'choose_date': {
+      return {
+        ...formState,
+        date: action.selectedDate,
+      }
+    }
+    case 'choose_time': {
+      return {
+        ...formState,
+        hours: action.selectedHour,
+      }
+    }
+    case 'clear_form': {
+      return {
+        ...formState,
+        hours: "17:00",
+        date: "",
+      }
+    }
+    default:
+      return formState;
+  }
+}
+
 function App() {
+
+  const [formState, dispatch] = useReducer(reducer, initialFormState);
+
+  const updateTime = (e) => {
+    dispatch({
+      type: "choose_time",
+      selectedHour: e.target.value,
+    })
+  };
+
+  const updateDate = (e) => {
+    dispatch({
+      type: "choose_date",
+      selectedDate: e.target.value,
+    })
+  }
+
+  const resetForm = () => {
+    dispatch({
+      type: "clear_form",
+    })
+  }
+
   return (
     <>
       <main>
         <Nav />
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/reservations" element={<Reservations
+            availableTimes={formState}
+            hours={hours}
+            selectedDateValue={updateDate}
+            selectedTimeValue={updateTime}
+            resetForm={resetForm}
+          />} />
         </Routes>
         <Footer />
       </main>
--- my-app/src/components/Reservations.js
+++ my-app/src/components/Reservations.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Reservations = () => {
+const Reservations = ({availableTimes, hours, selectedTimeValue, selectedDateValue, resetForm}) => {
+    const [guestNumber, setGuestNumber] = useState('');
+    const [occasionType, setOccasionType] = useState('');
+    const occasionOptions = ["Birthday", "Anniversary", "Promotion", "Engagement"];
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        resetForm();
+        setGuestNumber('');
+        setOccasionType('');
+    }
+
     return (
-        <section>
-            <h1>Reservation Component</h1>
+        <section className="reservations">
+            <div className="container">
+                <form className="reservationForm" onSubmit={handleSubmit}>
+                    <label htmlFor="res-date">Choose date</label>
+                    <input type="date" id="res-date" value={availableTimes.date} onChange={selectedDateValue} />
+                    <label htmlFor="res-time">Choose time</label>
+                    <select id="res-time " value={availableTimes.hours} onChange={selectedTimeValue}>
+                        {hours.map((hour) => {
+                            return <option key={hour} value={hour}>{hour}</option>;
+                        })}
+                    </select>
+                    <label htmlFor="guests">Number of guests</label>
+                    <input type="number" placeholder="Select" min="1" max="10" id="guests" value={guestNumber} onChange={e => setGuestNumber(e.target.value)} />
+                    <label htmlFor="occasion">Occasion</label>
+                    <select id="occasion" value={occasionType} onChange={e => setOccasionType(e.target.value)}>
+                        {occasionOptions.map((occasionOption) => {
+                            return <option key={occasionOption} value={occasionOption}>{occasionOption}</option>
+                        })}
+                    </select>
+                    <input type="submit" value="Make Your reservation" disabled={!availableTimes.date || !availableTimes.hours || !guestNumber} />
+                </form>
+            </div>
         </section>
     )
 }
